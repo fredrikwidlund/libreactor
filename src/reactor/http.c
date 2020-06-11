b924f892ac97050a52059cebeb59c3bb94e4d86d
Fredrik Widlund <fredrik.widlund@gmail.com>
2020-06-11 21:52:15 +0200

    http

--- src/reactor/http.c
+++ src/reactor/http.c
@@ -10,6 +10,54 @@
 #include "picohttpparser/picohttpparser.h"
 #include "reactor.h"
 
+static size_t http_chunk(segment data, segment *chunk)
+{
+  char *end;
+  size_t n;
+
+  end = memchr(data.base, '\n', data.size);
+  if (dynamic_unlikely(!end))
+    return 0;
+  *chunk = segment_data(end + 1, strtoul(data.base, NULL, 16));
+  n = (char *) chunk->base - (char *) data.base + chunk->size + 2;
+
+  return dynamic_unlikely(data.size < n) ? 0 : n;
+}
+
+static size_t http_dechunk(segment data, segment *dechunked)
+{
+  segment chunk;
+  size_t n, offset;
+
+  *dechunked = segment_data(data.base, 0);
+
+  offset = 0;
+  do
+    {
+      n = http_chunk(segment_offset(data, offset), &chunk);
+      if (dynamic_unlikely(n == 0))
+        return 0;
+      offset += n;
+    }
+  while (chunk.size);
+
+  offset = 0;
+  do
+    {
+      n = http_chunk(segment_offset(data, offset), &chunk);
+      offset += n;
+      memmove((char *) dechunked->base + dechunked->size, chunk.base, chunk.size);
+      dechunked->size += chunk.size;
+    }
+  while (chunk.size);
+
+  return offset;
+}
+
+/*************/
+/* http_date */
+/*************/
+
 segment http_date(int update)
 {
   time_t t;
@@ -18,7 +66,7 @@ segment http_date(int update)
   static const char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
   static __thread char date[30] = "Thu, 01 Jan 1970 00:00:00 GMT";
 
-  if (update)
+  if (dynamic_unlikely(update))
     {
       (void) time(&t);
       (void) gmtime_r(&t, &tm);
@@ -30,13 +78,37 @@ segment http_date(int update)
   return (segment) {date, 29};
 }
 
-static void http_headers_add(http_headers *headers, segment name, segment value)
+/****************/
+/* http_headers */
+/****************/
+
+void http_headers_construct(http_headers *headers)
 {
-  if (headers->count < HTTP_MAX_HEADERS)
+  headers->count = 0;
+}
+
+size_t http_headers_count(http_headers *headers)
+{
+  return headers->count;
+}
+
+void http_headers_add(http_headers *headers, segment name, segment value)
+{
+  if (dynamic_unlikely(headers->count < HTTP_MAX_HEADERS))
     {
       headers->header[headers->count] = (http_header) {name, value};
       headers->count ++;
     }
+}
+
+segment http_headers_lookup(http_headers *headers, segment name)
+{
+  size_t i;
+
+  for (i = 0; i < headers->count; i ++)
+    if (segment_equal_case(headers->header[i].name, name))
+      return headers->header[i].value;
+  return segment_empty();
 }
 
 static size_t http_headers_size(http_headers *headers)
@@ -71,18 +143,84 @@ static char *http_headers_write(http_headers *headers, char *base)
   return base;
 }
 
-ssize_t http_request_read(http_request *request, segment segment)
-{
-  ssize_t n;
+/****************/
+/* http_request */
+/****************/
 
-  request->body = (struct segment) {0};
+ssize_t http_request_read(http_request *request, segment data)
+{
+  segment value;
+  ssize_t n;
+  size_t header_size, body_size;
+
+  request->body = segment_empty();
   request->headers.count = HTTP_MAX_HEADERS;
-  n = phr_parse_request(segment.base, segment.size,
+  n = phr_parse_request(data.base, data.size,
                         (const char **) &request->method.base, &request->method.size,
                         (const char **) &request->target.base, &request->target.size,
                         &request->version,
                         (struct phr_header *) request->headers.header, &request->headers.count, 0);
-  return n == -2 ? 0 : n;
+  if (dynamic_likely(n > 0 && segment_equal(request->method, segment_string("GET"))))
+    return n;
+
+  if (n <= 0)
+    return n == -2 ? 0 : -1;
+  header_size = n;
+
+  value = http_headers_lookup(&request->headers, segment_string("Content-Length"));
+  if (value.size)
+    {
+      body_size = strtoull(value.base, NULL, 10);
+      request->body = segment_data((char *) data.base + header_size, body_size);
+      return header_size + body_size <= data.size ? header_size + body_size : 0;
+    }
+
+  if (segment_equal_case(http_headers_lookup(&request->headers, segment_string("Transfer-Encoding")),
+                         segment_string("Chunked")))
+    {
+      body_size = http_dechunk(segment_offset(data, header_size), &request->body);
+      return body_size ? header_size + body_size : 0;
+    }
+
+  return -1;
+}
+
+/*****************/
+/* http_response */
+/*****************/
+
+ssize_t http_response_read(http_response *response, segment data)
+{
+  segment value;
+  ssize_t n;
+  size_t header_size, body_size;
+
+  response->body = segment_empty();
+  response->headers.count = HTTP_MAX_HEADERS;
+  n = phr_parse_response(data.base, data.size,
+                         &response->version, &response->code,
+                         (const char **) &response->reason.base, &response->reason.size,
+                         (struct phr_header *) response->headers.header, &response->headers.count, 0);
+  if (dynamic_unlikely(n <= 0))
+    return n == -2 ? 0 : -1;
+  header_size = n;
+
+  value = http_headers_lookup(&response->headers, segment_string("Content-Length"));
+  if (value.size)
+    {
+      body_size = strtoull(value.base, NULL, 10);
+      response->body = segment_data((char *) data.base + header_size, body_size);
+      return header_size + body_size <= data.size ? header_size + body_size : 0;
+    }
+
+  if (segment_equal_case(http_headers_lookup(&response->headers, segment_string("Transfer-Encoding")),
+                         segment_string("Chunked")))
+    {
+      body_size = http_dechunk(segment_offset(data, header_size), &response->body);
+      return body_size ? header_size + body_size : 0;
+    }
+
+  return -1;
 }
 
 size_t http_response_size(http_response *response)
@@ -127,9 +265,9 @@ void http_response_construct(http_response *response,
      }
     };
 
-  if (type.size)
+  if (dynamic_likely(type.size))
     http_headers_add(&response->headers, segment_string("Content-Type"), type);
-  if (code < 100 || (code >= 200 && code != 204 && code != 304))
+  if (dynamic_likely(code < 100 || (code >= 200 && code != 204 && code != 304)))
     http_headers_add(&response->headers, segment_string("Content-Length"), utility_u32_segment(body.size));
 }
 
--- src/reactor/http.h
+++ src/reactor/http.h
@@ -40,12 +40,18 @@ struct http_response
 
 segment http_date(int);
 
+void    http_headers_construct(http_headers *);
+size_t  http_headers_count(http_headers *);
+void    http_headers_add(http_headers *, segment, segment);
+segment http_headers_lookup(http_headers *, segment);
+
+ssize_t http_request_read(http_request *, segment);
+
+ssize_t http_response_read(http_response *, segment);
 size_t  http_response_size(http_response *);
 void    http_response_write(http_response *, segment);
 void    http_response_construct(http_response *, int, int, segment, segment, segment);
 void    http_response_not_found(http_response *);
 void    http_response_ok(http_response *, segment, segment);
-
-ssize_t http_request_read(http_request *, segment);
 
 #endif /* HTTP_H_INCLUDED */
