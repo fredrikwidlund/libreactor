74c73e32d45fee45a0890e99fea3dd65e052aecc
Fredrik Widlund <fredrik.widlund@gmail.com>
2020-12-27 19:09:54 +0100

    update example

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM alpine:edge
-RUN apk add --update alpine-sdk jansson-dev
+RUN apk add --update alpine-sdk
 
-RUN wget https://github.com/fredrikwidlund/libdynamic/releases/download/v1.3.0/libdynamic-2.0.0.tar.gz && \
-    tar fvxz libdynamic-2.0.0.tar.gz && \
-    (cd libdynamic-2.0.0/ && ./configure --prefix=/usr AR=gcc-ar NM=gcc-nm RANLIB=gcc-ranlib && make install)
+RUN wget https://github.com/fredrikwidlund/libdynamic/releases/download/v2.2.0/libdynamic-2.2.0.tar.gz && \
+    tar fvxz libdynamic-2.2.0.tar.gz && \
+    (cd libdynamic-2.2.0/ && ./configure --prefix=/usr AR=gcc-ar NM=gcc-nm RANLIB=gcc-ranlib && make install)
 
-RUN wget https://github.com/fredrikwidlund/libreactor/releases/download/v1.0.0/libreactor-2.0.0.tar.gz && \
-        tar fvxz libreactor-2.0.0.tar.gz && \
-        (cd libreactor-2.0.0; ./configure --prefix=/usr AR=gcc-ar NM=gcc-nm RANLIB=gcc-ranlib; make install)
+RUN wget https://github.com/fredrikwidlund/libreactor/releases/download/v2.0.0-alpha/libreactor-2.0.0.tar.gz && \
+    tar fvxz libreactor-2.0.0.tar.gz && \
+    (cd libreactor-2.0.0; ./configure --prefix=/usr AR=gcc-ar NM=gcc-nm RANLIB=gcc-ranlib; make install)
--- example/server.c
+++ example/server.c
@@ -1,37 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include <signal.h>
 #include <err.h>
 
 #include <dynamic.h>
 #include <reactor.h>
 
-static core_status handler(core_event *event)
+typedef struct hello hello;
+struct hello
 {
-  server *server = event->state;
-  server_context *context = (server_context *) event->data;
+  server server;
+  timer  timer;
+  size_t requests;
+};
 
-  switch (event->type)
-  {
-  case SERVER_REQUEST:
-    server_ok(context, segment_string("text/plain"), segment_string("Hello, World!"));
-    return CORE_OK;
-  default:
-    warn("error");
-    server_destruct(server);
-    return CORE_ABORT;
-  }
+static core_status request(core_event *event)
+{
+  hello *hello = event->state;
+
+  if (event->type != SERVER_REQUEST)
+    err(1, "server");
+
+  server_ok((server_context *) event->data, segment_string("text/plain"), segment_string("Hello, World!"));
+  hello->requests++;
+  return CORE_OK;
+}
+
+static core_status timeout(core_event *event)
+{
+  hello *hello = event->state;
+  core_counters *counters;
+
+  if (event->type != TIMER_ALARM)
+    err(1, "timer");
+
+  counters = core_get_counters(NULL);
+  (void) fprintf(stderr, "[hello] requests %lu, usage %.02f%%, frequency %.02fGHz\n",
+                 hello->requests, 100. * (double) counters->awake / (double) (counters->awake + counters->sleep),
+                 (double) (counters->awake + counters->sleep) / 1000000000.0);
+  core_clear_counters(NULL);
+  hello->requests = 0;
+  return CORE_OK;
 }
 
 int main()
 {
-  server s;
+  hello hello = {0};
 
   signal(SIGPIPE, SIG_IGN);
   core_construct(NULL);
-  server_construct(&s, handler, &s);
-  server_open(&s, 0, 80);
+  server_construct(&hello.server, request, &hello);
+  server_open(&hello.server, 0, 80);
+  timer_construct(&hello.timer, timeout, &hello);
+  timer_set(&hello.timer, 1000000000, 1000000000);
   core_loop(NULL);
   core_destruct(NULL);
 }
