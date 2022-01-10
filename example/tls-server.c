085d5a046a20ed100154a84ebb685ff4db8a555d
Fredrik Widlund <fredrik.widlund@gmail.com>
2022-01-10 18:37:33 +0100

    cleanup examples

--- example/tls-server.c
+++ example/tls-server.c
@@ -2,7 +2,7 @@
 
 static void callback(reactor_event *event)
 {
-  server_transaction_text((server_transaction *) event->data, data_string("Hello, World!"));
+  server_ok((server_request *) event->data, data_string("text/plain"), data_string("Hello, World!"));
 }
 
 int main()
@@ -14,7 +14,7 @@ int main()
   server_construct(&s2, callback, NULL);
   server_open(&s1, net_socket(net_resolve("127.0.0.1", "80", AF_INET, SOCK_STREAM, AI_PASSIVE)), NULL);
   server_open(&s2, net_socket(net_resolve("127.0.0.1", "443", AF_INET, SOCK_STREAM, AI_PASSIVE)),
-              net_ssl_context("certs/cert.pem", "certs/key.pem"));
+              net_ssl_server_context("certs/cert.pem", "certs/key.pem"));
   reactor_loop();
   server_destruct(&s1);
   server_destruct(&s2);
